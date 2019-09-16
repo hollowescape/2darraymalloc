eaf1c71f18e7be9de2c87813c257128a99c917e5
Mayank Sharma <46193104+hollowescape@users.noreply.github.com>
2019-09-16 21:53:56 +0530

    Add files via upload

--- stud.c
+++ stud.c
@@ -1,5 +1,6 @@
 #include<stdio.h>
-  
+  int pos[50];
+
   typedef union deta
   {
   	char hostel[200];
@@ -38,10 +39,33 @@
                 }
             }
     }
-    void read(int n)
+    void search(pico a[],int x,int t,int p[])
+	{    int i;
+		for(i=0;i<t;i++)
+		{
+
+			if(a[i].roll==x)
+			{ printf("  the student%d details\n",(i+1));
+                printf("  the usn %d and rollno %d",a[i].usn,a[i].roll);
+                printf(" the name is %s",a[i].name);
+                printf(" date of birth is %d-%d-%d",a[i].d.date,a[i].d.mon,a[i].d.year);
+                if(p[i])
+                {
+                        printf(" hostel addhress is %s",a[i].y.hostel);
+                }
+                else
+                {
+                        printf(" home addhress is %s",a[i].y.home);
+                }
+			}
+		}
+	}
+
+
+    void read(pico s[],int n)
     {
-    	pico s[50];
-     	int c,pos[50];
+    	
+     	int c;
      	for(int i=0;i<n;i++)
      	{
 
@@ -73,9 +97,15 @@
     }
 
      void main(){
-     	int m;
+     	int m,h;
+	pico v[50];
+
      	printf(" enter the number of students\n");
      	scanf("%d",&m);
-     	read(m);
+     	read(v,m);
+	printf("\n enter the element to be searched\n");
+	scanf("%d",&h);
+	search(v,h,m,pos);
+
      }
 
